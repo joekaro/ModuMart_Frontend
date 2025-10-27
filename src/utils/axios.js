a8efce2e54e5ab2ae4a8cabfb156091ed889a022
joekaro <obanovwejoseph@gmail.com>
2025-10-27 07:35:06 +0100

    Enable withCredentials for axios instance

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:import.meta.env.VITE_API_URL,
+  withCredentials:true,
 });
 
 // ✅ Automatically attach token for both Admin and User safely
